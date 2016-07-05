0a907a058b61ce735d21062844cef94a24f53241
Håvard Ranum <havard.ranum@me.com>
2016-07-05 13:44:58 +0200

    feat(package) add webpack and react hot loader

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  eslint: {configFile: '.eslintrc'},
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:8080',
+    'webpack/hot/only-dev-server',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['react-hot', 'babel', 'eslint'],
+        include: path.join(__dirname, 'src')
+      }
+    ]
+  }
+};
