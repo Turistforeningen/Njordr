04ab9dac82e4982f77ce7e1454765c35f2b2f5e4
Håvard Ranum <havard.ranum@me.com>
2016-08-03 10:23:10 +0200

    feat(album) fetch album when selecting

--- src/containers/ArchiveContainer.jsx
+++ src/containers/ArchiveContainer.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 import Archive from '../components/Archive.jsx';
-import {selectAlbum} from '../actions/index.js';
+import {selectAlbum, fetchAlbum} from '../actions/index.js';
 
 const mapStateToProps = (state) => (Object.assign({}, state));
 
 const mapDispatchToProps = (dispatch) => ({
   selectAlbum: (id) => {
     dispatch(selectAlbum(id));
+    dispatch(fetchAlbum(id));
   },
 });
 
