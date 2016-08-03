6375b48aac3ea5012266c2a9153433760d8b79e2
Håvard Ranum <havard.ranum@me.com>
2016-08-03 10:25:08 +0200

    feat(album) show message while fetching photos

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -1,13 +1,14 @@
 import React, {PropTypes} from 'react';
 import Photo from './Photo.jsx';
 
-const Photos = ({album, photos}) => {
+const Photos = ({album, photos, isFetching}) => {
   if (photos.length === 0) {
     return (<div>Empty album!</div>);
   }
   return (
     <div>
       {album && album.name ? album.name : 'Album uten navn'}
+      {isFetching ? 'Henter bilder...' : ''}
       <ul>
         {photos.map(photo =>
           <Photo key={photo.id} {...photo} />
