5b5a9fc125cb1a6357927a89d51e315d35e51c86
Håvard Ranum <havard.ranum@me.com>
2016-09-23 10:24:41 +0200

    refactor(archive): links as anchor tags

--- src/components/Archive.jsx
+++ src/components/Archive.jsx
@@ -4,11 +4,10 @@ const Archive = ({albums, selectAlbum}) => (
   <div>
     Archive
     {(Object.keys(albums).map((key) => (
-      <div
-        key={key}
-        onClick={(e) => { selectAlbum(key); }}
-      >
-        {albums[key].name}
+      <div key={key}>
+        <a onClick={(e) => { selectAlbum(key); }}>
+          {albums[key].name}
+        </a>
       </div>
     )))}
   </div>
