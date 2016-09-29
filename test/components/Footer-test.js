59d4874faa174d7e7ac0f604ba8de0500aa7184c
Håvard Ranum <havard.ranum@me.com>
2016-09-29 13:47:20 +0200

    test(footer): add simple footer test

---
+++ test/components/Footer-test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {expect} from 'chai';
+import {shallow, mount, render} from 'enzyme';
+import {Footer} from '../../src/components/Footer.jsx';
+
+describe('Footer', function() {
+  it('renders two buttons which might be cancel and insert', function() {
+    expect(shallow(<Footer />).find('button')).to.have.length(2);
+  });
+});
