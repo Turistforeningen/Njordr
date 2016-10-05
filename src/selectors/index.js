cb9369212ddda91105e8107bb3f809d64f829bbd
Håvard Ranum <havard.ranum@me.com>
2016-10-05 15:12:08 +0200

    feat(selector): add selectedTagsSelector

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,1 +1,5 @@
 export const selectedAlbumSelector = state => state.albums[state.selectedAlbum];
+
+export const appliedTagsSelector = state => state.tags
+  .filter((item, index, list) => item.isApplied)
+  .map(item => item.val);
