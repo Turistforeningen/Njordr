1716b26d9110742b5cf47e2387d434c1a267da87
Håvard Ranum <havard.ranum@me.com>
2016-10-06 08:43:59 +0200

    feat(footer): let user preview and remove selected
    Preview and button for removing photo is now available in a popup
    visible on hover on each selected photo thumbnail

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,33 +1,68 @@
-import React, {PropTypes} from 'react';
+import React, {Component, PropTypes} from 'react';
+import ReactDOM from 'react-dom';
 import {connect} from 'react-redux';
+import $ from 'jquery';
+$.fn.popup = require('semantic-ui-popup');
+$.fn.transition = require('semantic-ui-transition');
 
-import {confirmSelection} from '../actions/index.js';
+import {confirmSelection, togglePhoto} from '../actions/index.js';
+import {photoThumbnailSelector} from '../selectors/index.js';
 
 // TODO: Create and use thumbnailSelector to select preview
-const SelectedPhoto = ({photo}) => (
-  <img className="ui circular image" src={photo.previews[8].href} />
-);
+class SelectedPhoto extends Component {
+  componentDidMount() {
+    const trigger = ReactDOM.findDOMNode(this.refs.trigger);
+    const $trigger = $(trigger);
+    $trigger.popup({hoverable: true});
+  }
 
-const SelectedPhotos = ({selectedPhotos}) => {
+  render() {
+    const {photo, removePhoto} = this.props;
+    const thumbnail = photoThumbnailSelector(photo);
+
+    return (
+      <span>
+        <img
+          className="ui mini circular image"
+          src={thumbnail}
+          ref="trigger"
+        />
+        <div className="ui popup top center">
+          <div className="header">{photo.id}</div>
+          <div className="content">
+            <img className="ui medium rounded bordered image" src={photo.previews[4].href} />
+            <button
+              className="ui red button"
+              onClick={() => { removePhoto(photo); }}>
+              Fjern fra valgte
+            </button>
+          </div>
+        </div>
+      </span>
+    );
+  }
+}
+
+const SelectedPhotos = ({selectedPhotos, removePhoto}) => {
   if (selectedPhotos.length === 0) {
     return <div>Du har ikke valgt noen bilder</div>;
   }
   return (
     <div>
-      <div className="ui mini images">
+      <div className="ui images">
         {selectedPhotos.map(photo => (
-          <SelectedPhoto key={photo.id} photo={photo} />
+          <SelectedPhoto key={photo.id} photo={photo} removePhoto={removePhoto} />
         ))}
       </div>
     </div>
   );
 };
 
-export const Footer = ({selectedPhotos, insertPhotos}) => (
+export const Footer = ({selectedPhotos, insertPhotos, removePhoto}) => (
   <footer className="ui bottom fixed menu">
     <div className="ui fluid container">
       <div className="left menu">
-        <SelectedPhotos selectedPhotos={selectedPhotos} />
+        <SelectedPhotos selectedPhotos={selectedPhotos} removePhoto={removePhoto} />
       </div>
       <div className="right menu">
         <button className="ui default button">Avbryt</button>
@@ -44,6 +79,9 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => ({
   insertPhotos: function insertPhotos() {
     dispatch(confirmSelection());
+  },
+  removePhoto: function removePhoto(photo) {
+    dispatch(togglePhoto(photo));
   },
 });
 
--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,5 +1,9 @@
 export const selectedAlbumSelector = state => state.albums[state.selectedAlbum];
 
+export const selectedPhotosSelector = state => state.app.selectedPhotos;
+
 export const appliedTagsSelector = state => state.tags
   .filter((item, index, list) => item.isApplied)
   .map(item => item.val);
+
+export const photoThumbnailSelector = photo => photo.previews[8].href;
