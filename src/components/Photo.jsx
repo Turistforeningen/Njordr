5645a2d0b4e3d50ceb5afab8568bd6e5a34edb22
Håvard Ranum <havard.ranum@me.com>
2016-10-11 11:52:42 +0200

    feat(photo): add popup with metadata

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,13 +1,22 @@
 import React, {Component, PropTypes} from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import moment from 'moment';
 $.fn.dimmer = require('semantic-ui-dimmer');
+$.fn.popup = require('semantic-ui-popup');
+$.fn.transition = require('semantic-ui-transition');
 
 class Photo extends Component {
   componentDidMount() {
+    // Setup photo blur
     const img = ReactDOM.findDOMNode(this.refs.image);
     const $img = $(img);
     $img.dimmer({on: 'hover'});
+
+    // Setup meta popup
+    const metaPopupTrigger = ReactDOM.findDOMNode(this.refs.meta);
+    const $metaPopupTrigger = $(metaPopupTrigger);
+    $metaPopupTrigger.popup({hoverable: true, lastResort: true});
   }
 
   render() {
@@ -49,6 +58,48 @@ class Photo extends Component {
               <i className="copyright icon"></i>
             </div> : ''}
           </div>
+        </div>
+        <div className="extra content">
+          <a className="left floated info" ref="meta">
+            <i className="info icon"></i>
+            Info
+          </a>
+          <div className="ui popup top center">
+            <div className="header">{id.length > 15 ? `${id.substring(0, 12)}...` : id}</div>
+            <div className="content">
+              <div className="ui list">
+                <div className="item">
+                  <i className="camera icon"></i>
+                  <div className="content">
+                    {photo.metadata.photographers.map((photographer, index) => (
+                      `
+                        ${photographer}
+                        ${index === (photo.metadata.photographers.length - 1) ? '' : ','}
+                      `
+                    ))}
+                  </div>
+                </div>
+                <div className="item">
+                  <i className="calendar icon"></i>
+                  <div className="content">
+                    {moment(photo.modified).format('DD.MM.YY')}
+                  </div>
+                </div>
+                <div className="item">
+                  <i className="tag icon"></i>
+                  <div className="content">
+                    {photo.metadata.tags.map((tag, index) => (
+                      `${tag}${index === (photo.metadata.tags.length - 1) ? '' : ','} `
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <span className="right floated select">
+            <i className="toggle off icon"></i>
+            Velg
+          </span>
         </div>
       </div>
     );
